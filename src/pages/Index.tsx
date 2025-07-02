3e18cea6f01b0c04ff593cae4655fd7bf8d0c2ee
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2025-07-02 20:00:43 +1000

    Comment out signup section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -41,7 +41,7 @@ const Index = () => {
           <BenefitsSection />
           <TeamSection />
           <TestimonialsSection />
-          <SignupSection />
+          {/* <SignupSection /> */}
         </main>
         
         <Footer />
